1a31c3c8a8fb319e7a3bdd53410b98ad29eecfe5
hlk <262864308@qq.com>
2014-09-08 22:10:10 +0800

    合并到一个类中

--- test/index.php
+++ test/index.php
@@ -26,9 +26,8 @@ INSERT INTO `CLICKS` (`CLICK_X`, `CLICK_Y`) VALUES (10, 10),(20, 20),(30, 30),(4
 //exit('Enlevez cette ligne pour que ce fichier marche/Remove this line to make this file work');
 
 include 'utils/Heatmap.class.php';
-include 'utils/HeatmapFromDatabase.class.php';
 
-$heatmap = new HeatmapFromDatabase();
+$heatmap = new Heatmap();
 /**
  * Requête (CLICK_Y se doit d'avoir un index pour de bonnes performances, voir EXPLAIN dans le manuel MySQL)
  * The query (CLICK_Y should have an index for good performances, see EXPLAIN in MySQL manual)
@@ -65,7 +64,7 @@ else
 	echo '<br /><br /><p style="line-height:0;">';
 	for ($i = 0; $i < $images['count']; $i++)
 	{
-		echo '<img src="', $images['filenames'][$i], '" width="', $images['width'], '" height="', $images['height'], '" alt="" /> Image ', $i, '<br />';
+		echo '<img src="', $images['filenames'][$i], '" width="', $images['width'], '" height="', $images['height'], '" alt="" /> Image ', $i, '<br /><br /><br /><br />';
 	}
 	echo '</p>';
 }
--- test/utils/Heatmap.class.php
+++ test/utils/Heatmap.class.php
@@ -27,8 +27,10 @@ class Heatmap
 	
 	/* @var boolean $palette Correctif pour la gestion de palette (cas des carrés rouges) / Correction for palette (in case of red squares) */
 	var $palette = false;
+	
 	/* @var boolean $alpha Valeur de transparence de l'image (par défaut pas de transparence) / Alpha level (default is no alpha) */
 	var $alpha = 0;
+	
 	/* @var boolean $rainbow Affichage de l'arc-en-ciel / Show rainbow */
 	var $rainbow = true;
 	/* @var boolean $copyleft Affichage du copyleft / Show copyleft */
@@ -60,6 +62,27 @@ class Heatmap
 	/* @var integer $__grey Niveau du gris (couleur du 0 clic) / Grey level (color of no-click) */
 	var $__grey = 240;
 
+	
+	//从数据库类中分离的变量
+	/** @var string $host Hôte (serveur) MySQL / MySQL host (server) */
+	var $host = 'localhost';
+	/** @var string $user Utilisateur MySQL / MySQL user */
+	var $user = '';
+	/** @var string $password Mot de passe de l'utilisateur MySQL / MySQL user's password */
+	var $password = '';
+	/** @var string $database Nom de la base de données MySQL / MySQL database name */
+	var $database = '';
+	/** @var integer $limit Limite du nombre de résultats renvoyés par la requête à chaque appel / Maximum number of results returned by each request call */
+	var $limit = 1000;
+	/** @var resource $link Lien (interne) MySQL / MySQL (internal) link */
+	var $link = false;
+	/** @var string $query Requête renvoyant les coordonnées des clics / Clicks coordinates query */
+	var $query = 'SELECT COORDS_X, COORDS_Y FROM CLICKS WHERE COORDS_Y BETWEEN %d AND %d';
+	/** @var string $maxQuery Requête renvoyant la coordonnées Y maximale / Max Y coordinate query */
+	var $maxQuery = 'SELECT MAX(COORDS_Y) FROM CLICKS';
+	
+	
+	
 	function Heatmap()
 	{
 		$this->alpha = min($this->alpha, 127);
@@ -173,6 +196,7 @@ class Heatmap
 			$dots[$i] = imagecreatetruecolor($this->dot, $this->dot);
 			imagealphablending($dots[$i], false);
 		}
+		
 		for ($x = 0; $x < $this->dot; $x++)
 		{
 			for ($y = 0; $y < $this->dot; $y++)
@@ -327,8 +351,8 @@ class Heatmap
 				}
 			}
 
-			$grey = imagecolorallocate($img, $this->__grey, $this->__grey, $this->__grey);
-			$gray = imagecolorallocate($img, ceil($this->__grey / 2), ceil($this->__grey / 2), ceil($this->__grey / 2));
+			$grey  = imagecolorallocate($img, $this->__grey, $this->__grey, $this->__grey);
+			$gray  = imagecolorallocate($img, ceil($this->__grey / 2), ceil($this->__grey / 2), ceil($this->__grey / 2));
 			$white = imagecolorallocate($img, 255, 255, 255);
 			$black = imagecolorallocate($img, 0, 0, 0);
 
@@ -379,6 +403,89 @@ class Heatmap
 	{
 		$this->error = $error;
 		return false;
+	}
+	
+	/**
+	 * Do some tasks before drawing (database connection...)
+	 **/
+	function startDrawing()
+	{
+		$this->link = @mysql_connect($this->host, $this->user, $this->password);
+		if ($this->link === false)
+		{
+			return $this->raiseError('Database connection error: '.mysql_error());
+		}
+		if (mysql_select_db($this->database) === false)
+		{
+			return $this->raiseError('Database selection error: '.$this->database);
+		}
+		$result = mysql_query($this->maxQuery);
+		if ($result === false)
+		{
+			return $this->raiseError('Query failed: '.mysql_error());
+		}
+		$max = mysql_fetch_row($result);
+		$this->maxY = $max[0];
+		mysql_free_result($result);
+		return true;
+	}
+	
+	/**
+	 * Find pixels coords and draw these on the current image
+	 *
+	 * @param integer $image Number of the image (to be used with $this->height)
+	 * @return boolean Success
+	 **/
+	function drawPixels($image)
+	{
+		$limit = 0;
+		do
+		{
+			/** Select with limit */
+			$result = mysql_query(sprintf($this->query, $image * $this->height, ($image + 1) * $this->height - 1).' LIMIT '.$limit.','.$this->limit);
+			if ($result === false)
+			{
+				return $this->raiseError('Query failed: '.mysql_error());
+			}
+			$count = mysql_num_rows($result);
+	
+			while ($click = mysql_fetch_row($result))
+			{
+				$x = (int) $click[0];
+				$y = (int) ($click[1]  - $image * $this->height);
+				if ($x < 0 || $x >= $this->width)
+				{
+					continue;
+				}
+				/** Apply a calculus for the step, with increases the speed of rendering : step = 3, then pixel is drawn at x = 2 (center of a 3x3 square) */
+				$x -= $x % $this->step - $this->startStep;
+				$y -= $y % $this->step - $this->startStep;
+				/** Add 1 to the current color of this pixel (color which represents the sum of clicks on this pixel) */
+				$color = imagecolorat($this->image, $x, $y) + 1;
+				imagesetpixel($this->image, $x, $y, $color);
+				$this->maxClicks = max($this->maxClicks, $color);
+				if ($image === 0)
+				{
+					/** Looking for the maximum height of click */
+					$this->maxY = max($y, $this->maxY);
+				}
+			}
+			/** Free resultset */
+			mysql_free_result($result);
+	
+			$limit += $this->limit;
+		} while ($count === $this->limit);
+		return true;
+	}
+	
+	/**
+	 * Do some cleaning or ending tasks (close database, reset array...)
+	 **/
+	function finishDrawing()
+	{
+		/** Close connection */
+		mysql_close($this->link);
+		return true;
 	}
 
 }
